31e0fe4298f4973bd01e888c7057c3ecfae07096
markwoodhall <mark.woodhall@gmail.com>
2013-05-02 20:08:07 +0100

    Initial Commit

---
+++ Scripts/JpSub/JpSub.Spec.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="jpsub.js" />
+
+describe("jpsub Events", function () {
+    var events;
+
+    beforeEach(function () {
+        events = new jpsub.events(new jpsub.eventStore({}));
+    });
+
+    it("should have a publish method", function () {
+        expect(events.publish).not.toBeUndefined();
+    });
+
+    it("should have a subscribe method", function() {
+        expect(events.subscribe).not.toBeUndefined();
+    })
+
+    it("should be possible to publish and subscribe to an event", function() {
+
+        var published = false; 
+
+        events.subscribe('Saved', function(data) { 
+            published = true;
+        })
+
+        events.publish('Saved', { data: "some data"});
+
+        expect(published).toBe(true);
+
+    })
+});
+
---
+++ Scripts/JpSub/JpSub.js
@@ -0,0 +1,66 @@
+﻿
+var jpsub = jpsub || {} ;
+
+jpsub.events = function(eventStore) {
+
+    var self = this; 
+
+    var _private = {
+        publish: function(event, data){
+            var events = _private.store.query(event);
+            for(var i = 0; i < events.length; i++){
+                events[i].callback(data);
+            }
+        },
+        subscribe: function(event, callback){
+            _private.store.add({eventName: event, callback: callback});
+        }
+    }
+
+    var _public = {
+        publish: function(event, data) {
+            _private.publish(event, data);
+        },
+        subscribe: function(event, callback) {
+            _private.subscribe(event, callback);
+        }
+    }
+
+    _private.store = eventStore; 
+
+    return _public;
+}
+
+jpsub.eventStore = function(container) {
+
+    var _private = {
+        store: undefined, 
+        init: function() {
+            container.jpsub = container.jpsub || {};
+            container.jpsub.events = container.jpsub.events || {};
+            container.jpsub.events.store = container.jpsub.events.store || [];
+            _private.store = container.jpsub.events.store;
+        },
+        add: function(event){
+            _private.store.push(event);
+        },
+        query: function(eventName){
+            var items = [];
+            for(var i = 0; i < _private.store.length; i++){
+                if(_private.store[i].eventName === eventName)
+                    items.push(_private.store[i]);
+            }
+            return items;
+        }
+    }
+
+
+    var _public = {
+        add: function(event) { _private.add(event); },
+        query: function(eventName) { return _private.query(eventName); }
+    }
+
+    _private.init();
+
+    return _public;
+}
